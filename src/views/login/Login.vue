7a0493ed1381869a3b76f6bd0c327895600b0526
franselles <fran.selles@gmail.com>
2020-03-10 16:03:32 +0100

    V-0-1-7

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -26,7 +26,7 @@
                   Login
                 </button>
               </div>
-              <a href="#" class="float-right">0.1.6</a>
+              <a href="#" class="float-right">0.1.7</a>
             </form>
           </div>
         </div>
